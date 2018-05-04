df339cae6ca7e5f6c8a6e287e2d6a59f754581d8
Thinh Le <ltthinh92@gmail.com>
2018-05-04 08:45:11 +0700

    Commit missing file

---
+++ Source/PQT.Domain/Concrete/EFEventSessionRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using PQT.Domain.Abstract;
+using PQT.Domain.Entities;
+
+namespace PQT.Domain.Concrete
+{
+    public class EFEventSessionRepository : Repository, IEventSessionRepository
+    {
+        public EFEventSessionRepository(DbContext db) : base(db)
+        {
+        }
+
+        public IEnumerable<EventSession> GetAllEventSessions()
+        {
+            return GetAll<EventSession>().AsEnumerable();
+        }
+
+        public EventSession GetEventSession(int id)
+        {
+            return Get<EventSession>(u => u.ID == id);
+        }
+
+        public EventSession CreateEventSession(EventSession info)
+        {
+            return TransactionWrapper.Do(() =>
+            {
+                return Create(info);
+            });
+        }
+
+        public bool UpdateEventSessuib(EventSession info)
+        {
+            return Update(info);
+        }
+
+        public bool DeleteEventSession(int id)
+        {
+            return Delete<Event>(id);
+        }
+    }
+}
