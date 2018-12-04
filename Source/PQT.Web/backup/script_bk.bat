e775cfd74a69b5bdd1554069cde76587a2328f95
LINH-PC\phanv <phanvulinhtn@gmail.com>
2018-12-04 13:54:06 +0700

    update script_bk

--- Source/PQT.Web/backup/script_bk.bat
+++ Source/PQT.Web/backup/script_bk.bat
@@ -1,4 +1,9 @@
 @echo off
+
+::for /F "tokens=1-4 delims=/ " %%i in ('date /t') do (set date=%%i) 
+for /f %%i in ('powershell ^(get-date^).DayOfWeek') do (set dow=%%i)
+
+
 set USER="root"
 set PASSWORD="priqua"
 set OUTPUT="D:\PQControlBackupTemp_NoDelete\db"
@@ -6,29 +11,30 @@ set OUTPUT2="D:\Dropbox\db"
 
 set INPUTDATA="C:\inetpub\wwwroot\PQT_Control\data"
 set OUTPUTDATA="D:\PQControlBackupTemp_NoDelete\data"
+set OUTPUTDATADOW="D:\PQControlBackupTemp_NoDelete\data\%dow%"
 set OUTPUTDATA2="D:\Dropbox\data"
 
 echo MySQL Backup wird gestartet...
 cd C:\Program Files\MySQL\MySQL Server 5.6\bin
-for /F "tokens=1-4 delims=/ " %%i in ('date /t') do (set date=%%i) 
-mkdir %OUTPUT%/%date%
+
+mkdir %OUTPUT%/%dow%
 echo 1/ mysqldump pqtdb...
-mysqldump -u%USER% -p%PASSWORD% -hlocalhost pqtdb > %OUTPUT%/%date%/pqtdb.sql
+mysqldump -u%USER% -p%PASSWORD% -hlocalhost pqtdb > %OUTPUT%/%dow%/pqtdb.sql
 echo Compressing... 
-"C:\Program Files (x86)\WinRAR\WinRAR.EXE" m -ppriqualive -r %OUTPUT%/%date%.rar %OUTPUT%/%date%
+"C:\Program Files (x86)\WinRAR\WinRAR.EXE" m -ppriqualive -r %OUTPUT%/%dow%.rar %OUTPUT%/%dow%
 
 echo copy bk file to dropbox...
 xcopy /s/y %OUTPUT% %OUTPUT2%
 
 echo Backup data resource wird gestartet...
-mkdir %OUTPUTDATA%/%date%
-xcopy /s/y %INPUTDATA% %OUTPUTDATA%/%date%
+mkdir %OUTPUTDATADOW%
+xcopy /s/y %INPUTDATA% %OUTPUTDATADOW%
 
 echo Compressing data ... 
-"C:\Program Files (x86)\WinRAR\WinRAR.EXE" m -ppriqualive -r %OUTPUTDATA%/%date%.rar %OUTPUTDATA%/%date%
+"C:\Program Files (x86)\WinRAR\WinRAR.EXE" m -ppriqualive -r %OUTPUTDATA%/%dow%.rar %OUTPUTDATADOW%
 
 echo copy bk file to dropbox...
 xcopy /s/y %OUTPUTDATA% %OUTPUTDATA2%
 
 echo Backup durchgeführt!
-
+pause
